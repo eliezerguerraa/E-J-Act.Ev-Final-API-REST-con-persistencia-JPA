6cc8096970d753b54e81fc164b7f1bfe1e9fdb9a
eliezerguerraa <eliezergrsr07@gmail.com>
2026-06-06 21:22:46 +0100

    estructura inicial

---
+++ src/test/java/com/proyecto/musica/MusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.musica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
